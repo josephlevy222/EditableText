30e8630e22139e2e267795af4f7634ab8cf25b95
josephlevy222 <josephlevy222@gmail.com>
2026-05-05 22:20:04 -0700

    -bottomPadding-keyboardHeight offset

--- Sources/EditableText/KeyboardScrollModifier.swift
+++ Sources/EditableText/KeyboardScrollModifier.swift
@@ -84,17 +84,17 @@ struct KeyboardScrollModifier: ViewModifier {
 								print("kH, kT, fTIB, bP", keyboardHeight, keyboardTop, focusedTextInputBottom, bottomPadding)
 							}
 							.animation(.easeOut, value: 0.16)
-						Color.red.frame(height: 1).offset(y: -bottomPadding).id("bottom")
+						Color.red.frame(height: 1).offset(y: -bottomPadding-keyboardHeight).id("target")
 							.padding(.bottom, -1)
 					}
 				}
 				.onChange(of: bottomPadding) { newHeight in
-					//if newHeight > 0, let id = registry.activeID {
+					if newHeight > 0/*, let id = registry.activeID */{
 						/// A slightly longer delay helps ensure the swap from Text to RichTextEditor is complete so the ID is attached to the new view.
 						DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
 							withAnimation(.easeInOut(duration: 5.3)) {
 								/// Using .top is more predictable than .center when the bottom half of the screen is "invisible."
-								proxy.scrollTo("bottom", anchor: .top)
+								proxy.scrollTo("target", anchor: .top)
 							}
 						}
 					//}
